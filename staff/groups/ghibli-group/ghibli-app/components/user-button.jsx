ba99e29b18d7504a896c97495cd78d550120d2ea
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-02-10 18:59:09 +0100

    fixed username on top nav

--- staff/groups/ghibli-group/ghibli-app/components/user-button.jsx
+++ staff/groups/ghibli-group/ghibli-app/components/user-button.jsx
@@ -1,7 +1,7 @@
 function UserButton (props) {
     return <div className="user-nav" id="nav">
         <div className="user" onClick={()=> props.showNav(props.toggleMenu)}>
-            <span className="login__text">SOFIA</span>
+            <span className="login__text">{props.user}</span>
             <div className="line"></div>
         </div>
         {props.toggleMenu===true && <UserNav watchlist={props.watchlist} editProfile={props.editProfile} logOut={props.logOut}/>}
